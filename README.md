fe639ec038304267dcfd3cccace556a850003d57
mDfRg <me@mindefrag.net>
2015-03-22 21:30:17 +0100

    modified README and correct autosleep version used

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# autosleep
+# Autosleep
 A simple bash script to automatically suspend idle home server to save electricity
+
+## How it works
+
+It measures an in/outbound network traffic and logs in given intervals. If the amount of data sent from the server don't exceeds a set limit, the server suspends itself. It's a very useful script intended to use in home servers that are not meant to be turned on constantly. 
+
+You can _wake_ your home server using **wakeonlan** tool by typing
+
+	wakeonlan [server's MAC address]
+
+And after the transmission (video streaming, file syncynig, torrent download) it will suspend itself.       
+
+
+## Setup
 
 On Debian based system you need to add a rule to sudoers file (sudo visudo):
 
 user ALL=(ALL) NOPASSWD: /usr/sbin/pm-suspend
+
+to be able to execute te script correctly. Then setup a cron job like this:
+
+*/15 **** sh /path/to/autosleep.sh
+
+## License
+
+This is just a modified version of a script I found in [Ubuntu Forums](http://ubuntuforums.org/showthread.php?t=530973&p=5195355#post5195355), originally posted by the user **vashwood**. I claim no copyright to it.
--- autosleep.sh
+++ autosleep.sh
@@ -6,7 +6,7 @@
 # If they haven't, it will force the system to sleep.
 #
 
-log=~/Scripts/idle/log
+log=/home/kostasz/scripts/idle/log
 
 # Extract the RX/TX
 rx=`/sbin/ifconfig eth0 | grep -m 1 RX | cut -d: -f2 | sed 's/ //g' | sed 's/errors//g'`
@@ -19,20 +19,20 @@ echo "rx: "$rx >> $log
 echo "tx: "$tx >> $log
 
 # Check if RX/TX Files Exist
-if [ -f ~/Scripts/idle/rx ] || [ -f ~Scripts/idle/tx ]; then
-	p_rx=`cat ~/Scripts/idle/rx`  ## store previous rx value in p_rx
-	p_tx=`cat ~/Scripts/idle/tx`  ## store previous tx value in p_tx
+if [ -f /home/kostasz/scripts/idle/rx ] || [ -f /home/kostasz/scripts/idle/tx ]; then
+	p_rx=`cat /home/kostasz/scripts/idle/rx`  ## store previous rx value in p_rx
+	p_tx=`cat /home/kostasz/scripts/idle/tx`  ## store previous tx value in p_tx
 	
 	echo "Previous Values" >> $log
 	echo "p_rx: "$p_rx >> $log
 	echo "t_rx: "$p_tx >> $log
 
-	echo $rx > ~/Scripts/idle/rx    ## Write packets to RX file
-	echo $tx > ~/Scripts/idle/tx    ## Write packets to TX file
+	echo $rx > /home/kostasz/scripts/idle/rx    ## Write packets to RX file
+	echo $tx > /home/kostasz/scripts/idle/tx    ## Write packets to TX file
 	
 	# Calculate threshold limit 
-	t_rx=`expr $p_rx + 1000`
-	t_tx=`expr $p_tx + 1000`
+	t_rx=`expr $p_rx + 3000`
+	t_tx=`expr $p_tx + 10000`
 
 	echo "Threshold Values" >> $log
 	echo "t_rx: "$t_rx >> $log
@@ -42,14 +42,14 @@ if [ -f ~/Scripts/idle/rx ] || [ -f ~Scripts/idle/tx ]; then
 	if [ $rx -le $t_rx ] || [ $tx -le $t_tx ]; then  ## If network packets have not changed that much
 		echo "Suspend to Ram ..." >> $log
 		echo " " >> $log
-		rm ~/Scripts/idle/rx
-		rm ~/Scripts/idle/tx
+		rm /home/kostasz/scripts/idle/rx
+		rm /home/kostasz/scripts/idle/tx
 		sudo pm-suspend  ## Force Sleep
 	fi
 	
 #Check if RX/TX Files Doesn't Exist
 else 
-	echo $rx > ~/Scripts/idle/rx ## Write packets to file
-	echo $tx > ~/Scripts/idle/tx
+	echo $rx > /home/kostasz/scripts/idle/rx ## Write packets to file
+	echo $tx > /home/kostasz/scripts/idle/tx
 	echo " " >> $log
 fi 
